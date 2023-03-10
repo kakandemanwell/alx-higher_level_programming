e54571019652ca7b5b4003338f7926b68324975d
kakandemanwell <kakandemanwell@gmail.com>
2023-03-09 18:49:21 -0800

    arguments as args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-from sys import argv as args
-
-if __name__ == '__main__':
-    if len(args) == 1:
-        print("0 arguments.")
-    elif len(args) == 2:
-        print("1 argument:")
-        print("1: {}".format(args[1]))
-    else:
-        print("{} arguments:".format(len(args) - 1))
-        for a in range(1, len(args)):
-            print("{}: {}".format(a, args[a]))
+if __name__ == "__main__":
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) is not 1 else "argument",
+                 "." if (size) is 0 else ":"))
+    for idx, arg in enumerate(userin):
+        print("{:d}: {:s}".format(idx + 1, arg))
