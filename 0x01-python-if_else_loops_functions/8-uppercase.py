d26d35004bc68340b82ab1f617c5c8d32851b7ef
kakandemanwell <kakandemanwell@gmail.com>
2023-03-19 11:10:40 -0700

    eliminated the else statement to keep the print statemens to just two

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,4 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             print(f"{chr(ord(i) - 32)}", end="")
-        else:
-            print("{}".format(i), end='')
     print("{}".format(''))
