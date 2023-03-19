48307ed7198b38a37ed73a5f0b13f86a6cab601b
kakandemanwell <kakandemanwell@gmail.com>
2023-03-19 11:01:28 -0700

    changed the bin/python link and format strings

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-for n in range(100):
+for n in range(99):
     print("{:02d}".format(n), end=', ')
+print("{}".format(99))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 
 for i in range(10):
     for j in range(10):
