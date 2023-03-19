3a0ccde51d7f16284e6bcef9f083d5798bcb4657
kakandemanwell <kakandemanwell@gmail.com>
2023-03-19 10:08:07 -0700

    fixed the Zero and extra lines

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,7 @@ else:
 if lst > 5:
     print("Last digit of {:d} is {} and is greater than 5".format(number, lst))
 elif lst == 0:
-    print("Last digit of {:d} is {} and is zero".format(number, lst))
+    print("Last digit of {:d} is {} and is 0".format(number, lst))
 elif lst < 6 and lst != 0:
-    print("Last digit of {: d} is {} and is less than 6 and not 0".format(number, lst))
+    print("Last digit of {: d} is {} ".format(number, lst))
+    print("and is less than 6 and not 0")
