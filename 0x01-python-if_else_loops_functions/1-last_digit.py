3da953c8a2948cbc7ca89d541bbc777d897a1378
kakandemanwell <kakandemanwell@gmail.com>
2023-03-19 10:15:18 -0700

    fixed the negative test case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     lst = number % 10
 else:
-    lst = (number % 10) * -1
+    lst = -(abs(number) % 10)
 
 if lst > 5:
     print("Last digit of {:d} is {} and is greater than 5".format(number, lst))
