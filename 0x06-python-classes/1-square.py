db42434593af936deb38d385a3fc55bca0b40da7
kakandemanwell <kakandemanwell@gmail.com>
2023-04-03 13:12:14 -0700

    first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+
+    """ defines a square by;
+        a private instance: size
+        instantiation with size(no type/value verification) """
+
+    def __init__(self, size):
+        
+        """ initializes the square class with a size parameter """
+
+        self._size = size
+
