c3941365bbe94d949699db6c3fe82bb1e40632e7
kakandemanwell <kakandemanwell@gmail.com>
2023-04-20 09:52:28 -0700

    changed operator

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Defines a Rectangle class."""
 
 
@@ -18,7 +17,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -31,7 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -42,12 +41,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    @method
     def area(self):
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        if self.__height == 0 or self.__width == 0:
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
             return (0)
-        else:
-            return (2 * (self.__width + self.__height))
+        return ((self.__width * 2) + (self.__height * 2))
