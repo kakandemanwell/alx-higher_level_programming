8eba196e4cbbc7463641055864af02fa83e82da6
kakandemanwell <kakandemanwell@gmail.com>
2023-04-20 09:49:27 -0700

    changed operator

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
         return (self.__width * self.__height)
 
     def perimeter(self):
-        if self.__height or self.__width is 0:
-            return 0
+        if self.__height == 0 or self.__width == 0:
+            return (0)
         else:
             return (2 * (self.__width + self.__height))
