04afea6506c38a47b424e010c1c07ac7f31c4774
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-02-22 07:00:46 -0800

    initial commit

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == '__main__':
-    from sys import argv
-    result = 0
-    if len(argv) > 1:
-        for a in range(1, len(argv)):
-            result += int(argv[a])
-    print(result)
--- 0x02-python-import_modules/README.md
+++
@@ -1,2 +0,0 @@
-this repo holds solutions to the modules assignment.
-with a total of 10 files.
