d6f48b555dd3ef43acd95b3268aace725412777d
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-02-24 18:14:33 -0800

    made extra changes and fixed code to suite requirements

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
+    """deletes an item at a position in the list"""
     if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    updated_list = my_list[:idx] + my_list[idx + 1:]
-    return updated_list
+        del my_list[idx]
+    return my_list
