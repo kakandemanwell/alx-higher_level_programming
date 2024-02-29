69557e80f7cd62909535d348b86206f35ed432bc
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-02-28 19:11:15 -0800

    initial commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,5 @@ def square_matrix_simple(matrix=[]):
     squared_matrix = []
 
     for list in matrix:
-        squared_lisit = []
-        for element in list:
-            squared_list.append(element**2)
-        squared_matrix.append(squared_list)
-    return squared_matrix
+        squared_matrix.append([element**2 for element in list)
+        return squared_matrix
