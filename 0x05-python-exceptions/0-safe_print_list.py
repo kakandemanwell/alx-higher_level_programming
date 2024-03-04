4e0d1b4914efb84997f92a5fe5ccb7be7a713529
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-03-04 03:46:32 -0800

    changed 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,12 +8,13 @@ def safe_print_list(my_list=[], x=0):
     return:
         number of printed elements
     """
-    n = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]))
-            n += 1
-        except IndexError:
-            break
-    print(" ")
-    return(n)
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end=' ')
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return count
