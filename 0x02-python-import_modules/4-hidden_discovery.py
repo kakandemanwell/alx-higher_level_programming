2e805278aad8e566159074adde2c4845fd4ad8db
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-03-15 17:15:56 -0700

    added sys module in imports

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+import sys
+import hidden_4 as hidden
 
-if __name__ == "__main__":
-    """prints all the names defined by the compiled module hidden_4.pyc"""
-    import hidden_4
-    import sys
+if __name__ != "__main__":
+    exit()
 
-    for nem in dir(hidden_4):
-        if nem[0:2] != "__":
-            print(nem)
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
