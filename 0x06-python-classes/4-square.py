0475f1e76e94719de753e1dd823ec8d2600be42b
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-03-19 04:31:27 -0700

    changed type checking for the size variable

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """ defines a class square"""
 
-
 class Square:
-    """Defines a square instance of size size."""
-    def __init__(self, size):
+    """Represents a square"""
+
+    def __init__(self, size=0):
         """
-        Initializes the Square with a sze.
+        Initialize a new square.
 
-        Parameters:
-        size: (int): The size of the square. Default is 0.
-
-        Raises:
-        TypeError: if size is not an integer.
-        ValueError: if size is less than 0.
+        prameters:
+        size (int): The size of the square.
         """
         self.__size = size
 
     @property
     def size(self):
-        """returns the size"""
-        return self.__size
+        """sets the size of the square"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """sets the size to arg value"""
-        if (type(value) is not int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif (value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """returns the area of the class object"""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size ** 2)
