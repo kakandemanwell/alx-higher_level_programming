d70fe67f4917b62bc68a42a3f758734fba72475e
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-04-04 19:48:48 -0700

    fixed typos in the code

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS second_table;
 CREATE TABLE second_table (id INTEGER, name VARCHAR(256), score INTEGER);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
+INSERT INTO `second_table` (id, name, score) VALUES
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
