14195f5a087c49b7b11a9a527d613d31d299ff06
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-04-11 16:01:41 -0700

    Fixed Casing

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 
 USE hbtn_od_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM  tv_shows
+FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
