5261469ea3e17ff5d24bdbb05f4ed986be499a3d
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-04-11 15:57:58 -0700

    Initial Commit

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM  tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-order by title, genre_id;
+order by tv_shows.title, tv_show_genres.genre_id;
