b4cd9da20793b8d7303807e890a808d8e54eb3ad
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-04-16 16:37:23 -0700

    Fixed typo in 6-print_soteed_dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        print(key, ":", a_dictionary[key])
+    key_list = list(a_dictionary.keys())
+    key_list.sort()
+
+    for i in key_list:
+        print("{} : {}".format(i, a_dictionary.get(i)))
