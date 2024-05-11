5c73e575b972f98bbac03a42ccd439325e905f01
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-05-11 01:50:02 -0700

    fixed import

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-
-// Import the list from 100-data.js
-const { list } = require('./100-data.js').list;
-// Compute the new list by multiplying each value by its index
+const list = require('./100-data.js').list;
 const newList = list.map((value, index) => value * index);
 
-// Print the original list and the new list
 console.log(list);
 console.log(newList);
