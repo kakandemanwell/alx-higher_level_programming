51af7d5c9c651fbeb46d05645b53c9ca3d1c7048
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-05-11 15:57:55 -0700

    removed extra imports

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,8 +5,7 @@ takes in a URL, sends a request to the URL and displays the body
 of the response (decoded in utf-8).
 """
 
-import urllib.request
-import urllib.error
+import urllib
 import sys
 
 if __name__ == "__main__":
