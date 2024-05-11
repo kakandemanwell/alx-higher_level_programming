416cb0726bda9895999bf30d99535ccb59d7ce98
Emmanuel Kakande <kakandemanwell@gmail.com>
2024-05-11 15:59:12 -0700

    removed extra imports

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-
 """
-takes in a URL, sends a request to the URL and displays the body
-of the response (decoded in utf-8).
+takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 """
 
-import urllib
 import sys
+from urllib import request, error
 
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+    url = sys.argv[1]
+
+    own_request = request.Request(url)
     try:
-        with urllib.request.urlopen(req) as response
-            print(response.read().decode('utf-8'))
-    except urllib.error.URLError as e:
+        with request.urlopen(own_request) as response:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
         print("Error code: {}".format(e.code))
