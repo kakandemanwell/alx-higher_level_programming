aa170a429ab9883357a9a455943950faf8ba397c
Manwell <kakandemanwell@gmail.com>
2024-05-31 00:41:09 +0300

    changed typo in zero divion error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-def list_division(my_list1, my_list2, list_length):
-    """
-    args:
-    my_list1 (list): a list to divide
-    my_list2 (list): a list to divide(denominator)
-    return:
-    returns a new_list with division results
-    """
-    new_list = []
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
     for i in range(list_length):
         try:
-            div = my_list1[i] / my_list2[i]
-        except TypeError:
-            print("wrong type")
-            div = 0
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                print("out of range")
+                result_list.append(0)
+            else:
+                a = my_list_1[i]
+                b = my_list_2[i]
+                if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+                    print("wrong type")
+                    result_list.append(0)
+                else:
+                    result_list.append(a / b)
         except ZeroDivisionError:
-            print("division by zero")
-            div = 0
-        except IndexError:
-            div = 0
-        finally:
-            new_list.append(div)
-    return (new_list)
+            print("division by 0")
+            result_list.append(0)
+        except Exception as e:
+            print("unexpected error:", e)
+            result_list.append(0)
+    
+    return result_list
