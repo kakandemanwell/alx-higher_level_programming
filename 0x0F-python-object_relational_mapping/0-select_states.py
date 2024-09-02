7d041d6e5ab62b09f01e8807a89ad7b4bab51dd6
kakandemanwell <kakandemanwell@gmail.com>
2024-09-02 06:16:19 -0700

    Changed as import for mysqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import MySQLdb as db
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    session = db.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name, charset="utf8")
+    session = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name, charset="utf8")
 
     cursor = session.cursor()
 
