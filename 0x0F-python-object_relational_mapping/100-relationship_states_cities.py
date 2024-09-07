149b1975f7494babfb0161b7b9ed2c21ae9687bf
kakandemanwell <kakandemanwell@gmail.com>
2024-09-07 11:44:10 -0700

    Fixed indentation on line 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,8 +11,8 @@ from relationship_city import City
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"\
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Base.metadata.create_all(engine)
 
